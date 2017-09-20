28fef2f834f2a1a9ce16d8504383f716705d837f
Craig Cravath <craigcravath@gmail.com>
2017-09-20 14:16:33 -0400

    change .vf file to be .venv to align with autogenerated Python .gitignore file

--- functions/vEnvManager.fish
+++ functions/vEnvManager.fish
@@ -3,9 +3,9 @@ function vEnvManager --on-variable PWD
     if test -n "$V_ENV_DIR"
         string match --regex {$V_ENV_DIR}.\* (pwd); and return; or vf deactivate; and set -ex V_ENV_DIR; and set -ex V_ENV_NAME
     end
-    if test -e .vf
+    if test -e .venv
         echo 'Virtual Env Exists for this project'
-        set -gx V_ENV_NAME (head -n 1 .vf)
+        set -gx V_ENV_NAME (head -n 1 .venv)
         echo $V_ENV_NAME
         set -gx V_ENV_DIR (pwd)
         vf activate $V_ENV_NAME
